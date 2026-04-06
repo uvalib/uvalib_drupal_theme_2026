c435cfb608c37e93991ea9298dce28a4056a318f
Xiaoming on library-drupal-staging-0 <xw5d@virginia.edu>
2026-04-06 20:34:09 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# UVa Drupal Theme 2026 based on Bootstrap 5 - Bootstrap SASS Starter Kit
---
+++ js/custom.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.uvalib_drupal_theme_2026 = {
+    attach: function(context, settings) {
+
+      // Custom code here
+
+    }
+  };
+
+})(jQuery, Drupal);
